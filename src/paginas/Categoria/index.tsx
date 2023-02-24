3c8f9f7d3876834f789b294f297ed87644dc74d4
Larissa Itimura <litimura@ciandt.com>
2023-02-23 21:41:54 -0300

    Adding the components for the website

---
+++ src/paginas/Categoria/index.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Loader from "../../componentes/Loader";
+import TituloPrincipal from "../../componentes/TituloPrincipal"
+import http from "../../http";
+import { ICategoria } from "../../interfaces/ICategoria";
+
+const Categoria = () => {
+    const [categoria, setCategoria] = useState<ICategoria>();
+    const [estaCarregando, setEstaCarregando] = useState<boolean>(true);
+    const params = useParams();
+
+    useEffect(() => {
+        setEstaCarregando(true);
+        http.get<ICategoria[]>('categorias', {
+            params: {
+                slug: params.slug
+            }
+        }).then(resposta => {
+            setCategoria(resposta.data[0])
+            setEstaCarregando(false);
+        })
+    },[params.slug]);
+
+    if (estaCarregando) {
+        return <Loader />
+    }
+
+    return (
+        <section>
+            <TituloPrincipal texto={categoria?.nome ?? ''}/>
+        </section>
+    )
+}
+
+export default Categoria;
